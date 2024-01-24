1b684b0d1369686458a3aca56d951b7cf8672ad3
cute1 <cute1@ANTHONY>
2024-01-23 18:32:43 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,121 @@
+﻿//this is a single-line comment
+/* this is a multi-line comment
+ * 
+ * Author:  Anthony Alvarez
+ * Course:  COMP-003A-L01
+ * Purpose: Code for Assignment 1
+ */
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. I am 21");
+            Console.WriteLine("2. I am in college");
+            Console.WriteLine("3. I am in my 3rd year of college");
+            Console.WriteLine("4. I workout sometimes");
+            Console.WriteLine("5. my favorite food is a big steak");
+            Console.WriteLine("6. I dont have a girlfriend");
+            Console.WriteLine("7. I used to have a girlfriend");
+            Console.WriteLine("8. I drink with my friends sometimes");
+            Console.WriteLine("9. My favorite game right now is rainbow six siege");
+            Console.WriteLine("10. My favorite color is blue");
+            Console.WriteLine("11. I have 3 dogs");
+            Console.WriteLine("12. I used to have 5 dogs");
+            Console.WriteLine("13. One name is Ariel");
+            Console.WriteLine("14. Another is Smoke");
+            Console.WriteLine("15. Another is Duke");
+            Console.WriteLine("16. Another is LB");
+            Console.WriteLine("17. Another is Mocha");
+            Console.WriteLine("18. Another is Lady");
+            Console.WriteLine("19. The last one is Yuki");
+            Console.WriteLine("20. I have 4 family members");
+            Console.WriteLine("21. Me");
+            Console.WriteLine("22. My step dad");
+            Console.WriteLine("23. My Mom");
+            Console.WriteLine("24. My sister");
+            Console.WriteLine("25. I play alot of video games");
+            Console.WriteLine("26. I own a PC");
+            Console.WriteLine("27. My first pc broke");
+            Console.WriteLine("28. I spilled water on it");
+            Console.WriteLine("29. My new pc is a year old");
+            Console.WriteLine("30. One of my friends name is AJ");
+            Console.WriteLine("31. One of my friends name is John");
+            Console.WriteLine("32. One of my friends name is Eyvi");
+            Console.WriteLine("33. One of my friends name is Richy");
+            Console.WriteLine("34. One of my friends name is Emilio");
+            Console.WriteLine("35. One of my friends name is Chubs");
+            Console.WriteLine("36. One of my friends name is Roque");
+            Console.WriteLine("37. One of my friends name is Eggword");
+            Console.WriteLine("38. One of my friends name is Kiko");
+            Console.WriteLine("39. One of my friends name is Juffy");
+            Console.WriteLine("40. One of my friends name is Rodrigo");
+            Console.WriteLine("41. One of my friends name is Jordan");
+            Console.WriteLine("42. One of my friends name is Jason");
+            Console.WriteLine("43. One of my friends name is Eddy");
+            Console.WriteLine("44. One of my friends name is Angel");
+            Console.WriteLine("45. One of my friends name is Ally");
+            Console.WriteLine("46. One of my friends name is Eva");
+            Console.WriteLine("46. One of my friends name is Rome");
+            Console.WriteLine("47. One of my friends name is Jacob");
+            Console.WriteLine("48. One of my friends name is Yovany");
+            Console.WriteLine("49. One of my friends name is Mateo");
+            Console.WriteLine("50. One of my friends name is Issac");
+            Console.WriteLine("51. One of my friends name is Carlos");
+            Console.WriteLine("52. One of my friends name is Victor");
+            Console.WriteLine("53. One of my friends name is Isaiah");
+            Console.WriteLine("54. One of my friends name is Emily");
+            Console.WriteLine("55. One of my friends name is Ken");
+            Console.WriteLine("56. One of my friends name is Victor Cortez");
+            Console.WriteLine("57. I have alot of family Members");
+            Console.WriteLine("58. One of my family members name is Bobe");
+            Console.WriteLine("59. One of my family members name is Anthony");
+            Console.WriteLine("60. One of my family members name is Raul");
+            Console.WriteLine("61. One of my family members name is Marie");
+            Console.WriteLine("62. One of my family members name is Gloria");
+            Console.WriteLine("63. One of my family members name is Richard");
+            Console.WriteLine("64. One of my family members name is Kathy");
+            Console.WriteLine("65. One of my family members name is Matt");
+            Console.WriteLine("66. One of my family members name is Alexis");
+            Console.WriteLine("67. One of my family members name is Momo");
+            Console.WriteLine("68. One of my family members name is Mersedies");
+            Console.WriteLine("69. One of my family members name is Kamree");
+            Console.WriteLine("70. One of my family members name is Audrey");
+            Console.WriteLine("71. One of my family members name is Bentley");
+            Console.WriteLine("72. One of my family members name is Christian");
+            Console.WriteLine("73. One of my family members name is Melissa");
+            Console.WriteLine("74. One of my family members name is Chris");
+            Console.WriteLine("75. One of my family members name is Bubba");
+            Console.WriteLine("76. One of my family members name is Chocho");
+            Console.WriteLine("77. One of my family members name is Jasmine");
+            Console.WriteLine("78. One of my family members name is Becky");
+            Console.WriteLine("79. One of my family members name is Javi");
+            Console.WriteLine("80. One of my family members name is BeeBear");
+            Console.WriteLine("81. One of my family members name is Eedies");
+            Console.WriteLine("82. One of my family members name is Alexandra");
+            Console.WriteLine("83. One of my family members name is Alex");
+            Console.WriteLine("84. One of my family members name is Gavin");
+            Console.WriteLine("85. One of my family members name is Val");
+            Console.WriteLine("86. One of my family members name is Briele");
+            Console.WriteLine("87. One of my family members name is Tony");
+            Console.WriteLine("88. One of my family members name is Christine");
+            Console.WriteLine("89. One of my family members name is Jennifer");
+            Console.WriteLine("90. One of my family members name is Valerie");
+            Console.WriteLine("91. One of my family members name is Kobe");
+            Console.WriteLine("92. One of my family members name is Jordan");
+            Console.WriteLine("93. One of my family members name is Jayleen");
+            Console.WriteLine("94. One game I play is Rainbow six siege");
+            Console.WriteLine("95. My rank in siege is emerald");
+            Console.WriteLine("96. I play Overwatch 2");
+            Console.WriteLine("97. My rank in overwatch is Diamond");
+            Console.WriteLine("98. I play Valorant");
+            Console.WriteLine("99. My rank in Valorant is Gold");
+            Console.WriteLine("100. I love Ice cream");
+
+
+
+        }
+    }
+}
